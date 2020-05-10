62aefd174aa90ebae9bf5d0ee62292b54d4880d3
frankluongo <frank@frankluongo.com>
2020-05-10 18:24:55 -0400

    Asset Management | Images

---
+++ README.md
@@ -0,0 +1,11 @@
+# Webpack Demo
+
+## Asset Management
+
+### CSS
+
+### Images
+
+Check out:
+- [Image Webpack Loader](https://github.com/tcoopman/image-webpack-loader)
+- [URL Loader](https://webpack.js.org/loaders/url-loader/)
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from "lodash";
 
 import "./style.css";
+import testImage from "./image.jpg";
 
 function component() {
   const element = document.createElement("div");
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add("hello");
+
+  const myImage = new Image();
+  myImage.src = testImage;
+
+  element.appendChild(myImage);
 
   return element;
 }
