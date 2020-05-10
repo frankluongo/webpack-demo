a426044c223d1e4709d13575b23e8b893a0dea23
frankluongo <frank@frankluongo.com>
2020-05-10 18:34:26 -0400

    Asset Management | Wrap Up

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 import _ from "lodash";
 
-import "./style.css";
-import testImage from "./image.jpg";
-import Data from "./data.xml";
-
 function component() {
   const element = document.createElement("div");
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
-  const myImage = new Image();
-  myImage.src = testImage;
-
-  element.appendChild(myImage);
-
-  console.log(Data);
 
   return element;
 }
