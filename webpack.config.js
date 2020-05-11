98cd150d6d351a48e22f851830420b5ddbb87859
frankluongo <frank@frankluongo.com>
2020-05-11 08:19:39 -0400

    HMR | Enabling HMR

--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,28 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  // mode: "development",
   devtool: "inline-source-map",
   entry: {
     app: "./src/index.js",
-    print: "./src/print.js",
   },
   devServer: {
     contentBase: "./dist",
+    hot: true,
   },
   plugins: [
-    new CleanWebpackPlugin({
-      cleanStaleWebpackAssets: false,
-    }),
+    // new CleanWebpackPlugin({
+    //   cleanStaleWebpackAssets: false
+    // }),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Output Management",
+      title: "Hot Module Replacement",
     }),
   ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     // This makes sure that files are served correctly when using the custom server
-    publicPath: "/",
+    // publicPath: "/",
   },
 };
